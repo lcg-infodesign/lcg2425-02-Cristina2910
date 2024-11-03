c44304d436b082c044fad65e53b9a959dc5db800
Cristina2910 <cristina.pedrazzini@mail.polimi.it>
2024-11-03 18:06:13 +0100

    assignment 2

--- sketch.js
+++ sketch.js
@@ -1,17 +1,65 @@
-function preload() {
-  // put preload code here
-}
+let righe = 12; // numero di righe
+let colonne = 12; // numero di colonne
+let cellWidth, cellHeight;
 
+// la dimensione del canvas dipende dalla dimensione della finestra
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  createCanvas(windowWidth, windowHeight); 
+  cellWidth = width / colonne;
+  cellHeight = height / righe;
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background("white");
+  for (let row = 0; row < righe; row++) {
+    for (let col = 0; col < colonne; col++) {
+      // coordinate della cella corrente
+      let x = col * cellWidth + cellWidth / 2;
+      let y = row * cellHeight + cellHeight / 2;
+      push();
+      translate(x, y);
+      
+      // applico una trasformazione ad ogni glifo
+      let rotation = random(TWO_PI);
+      let scaleFactor = random(0.5, 1);
+      rotate(rotation);
+      scale(scaleFactor);
+      
+      // disegno il glifo principale con le trasformazioni
+      drawGlyph();
+      pop();
+    }
+  }
+}
+
+function drawGlyph() {
+  //mette un colore casuale come riempimento
+  fill(random(255), random(255), random(255));
+  
+  stroke(200, 162, 200); //lilla
+  strokeWeight(2);
+  //linea centrale
+  line(-10, 0, 10, 0);
+
+  //cerchi agli estremi
+  fill(random(255), random(255), random(255)); //mette un colore casuale nei cerchi
+  ellipse(-10, 0, 5, 5);
+  ellipse(10, 0, 5, 5);
+
+  //linea verticale
+  line(0, -10, 0, 10);
+}
+
+//il canvas si ridimensiona quando si ridimensiona la finestra
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); //ridimensiona il canvas
+  cellWidth = width / colonne; //ricalcola la larghezza delle celle
+  cellHeight = height / righe; //ricalcola l'altezza delle celle
+  redraw(); //ridisegna il canvas
+}
+
+//il disegno si aggiorna al click del mouse
+function mousePressed() {
+  redraw();
 }
